d33a1573f631b5e6d4b458a2531d3cdfef5192a0
Nikita Smagin <nikita.s@prmnd.com>
2022-07-26 23:30:08 +0500

    add icon

---
+++ components/icon/index.vue
@@ -0,0 +1,47 @@
+<template>
+  <component
+      :is="iconsMap[props.type]"
+      :width="props.width || 'auto'"
+      :height="props.height || 'auto'"
+      :style="{
+      'min-width': props.width ? `${props.width}px` : 'auto',
+      'min-height': props.height ? `${props.height}px` : 'auto',
+    }"
+      role="presentation"
+      v-bind="$attrs"
+      :color="props.color"
+      @click="$emit('click', { props: $props, from: null })"
+  />
+</template>
+
+
+<script setup lang="ts">
+import Heart from '@/components/icon/icons/heart.vue'
+import Box from '@/components/icon/icons/box.vue'
+import Geo from '@/components/icon/icons/geo.vue'
+import Bag from '@/components/icon/icons/bag.vue'
+import Search from '@/components/icon/icons/search.vue'
+
+
+interface Props {
+  width?:string,
+  height?:string,
+  color?:string,
+  type?:string
+}
+const props = defineProps<Props>()
+const iconsMap = {
+  heart: Heart,
+  box: Box,
+  geo: Geo,
+  bag: Bag,
+  search: Search,
+
+}
+</script>
+
+<style scoped>
+svg {
+  display: block;
+}
+</style>
