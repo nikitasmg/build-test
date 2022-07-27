b590b8e55b05ac0b055f9082e1eeb0bf70dd96b7
Nikita Smagin <nikita.s@prmnd.com>
2022-07-27 18:09:22 +0500

    delete unused enums

--- core/enums/index.ts
+++ core/enums/index.ts
@@ -4,20 +4,10 @@ export enum Colors {
     SUCCESS = 'success',
 }
 
-export enum Actions {
-    ADD = 'add',
-    BUY = 'buy',
-}
-
 export enum ProductActions {
     ADD_TO_FAVORITE= 'addToFavorite',
     ADD_TO_DEALS= 'addToDeals',
     BUY_PRODUCT= 'BuyProduct',
-}
-export enum TypeOfPage {
-    ALL= 'all',
-    DEALS='deals',
-    FAVORITE='favorite',
 }
 
 export enum Routes {
